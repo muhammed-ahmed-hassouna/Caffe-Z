4a7a35fdd1f67b8db3ea73baa1e9b063ea5877e9
muhammed-ahmed-hassouna <psmohammad780@gmail.com>
2023-10-01 17:50:02 +0300

    edit

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
-let ask = prompt("What's you'r name?");
-let ask2 = prompt("What's you'r gender? (Male or Female)");
+let name0 = prompt("What's you'r name?");
+let gender;
+let array = [];
 
-if (ask2 == "Male") {
-    alert("Welcome Mr " + ask);
-}
-else if (ask2 == "Female") {
-    alert("Welcome Ms " + ask);
-}
-else {
-    alert("Welcome");
-}
-let ask3 = prompt("Do you want a cold or hot drink?");
-let ask4 = prompt("What is the name of the drink you want?");
-alert("You'r drink " + ask4 + "is getting prepared")
 
-console.log(ask +" Ordered a " +ask3 + " " +ask4)
+function myfuction () {
 
+while (gender != "male" || gender != "female") {
+    gender = prompt("What's you'r gender? (Male or Female)");
+    let b = gender.toLocaleLowerCase();
+    if (b == "male") {
+        alert("Welcome Mr " + name0);
+        array.push(name0);
+        array.push(b);
+        break;
+    }
+    else if (b == "female") {
+        alert("Welcome Ms " + name0);
+        array.push(name0);
+        array.push(b);
+        break;
+
+    }
+}
+
+let type = prompt("Do you want a cold or hot drink?");
+array.push(type);
+
+let namedrink = prompt("What is the name of the drink you want?");
+array.push(namedrink);
+alert("You'r drink " + namedrink + " is getting prepared");
+
+console.log(name0 + " ordered a " + type + " " + namedrink);
+
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
+}
+
+myfuction ();
